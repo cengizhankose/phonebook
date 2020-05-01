40ac6367b5ebae06ad1b628cbfed87577bb8ad96
Groed <kose651@gmail.com>
2020-05-01 22:12:55 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,12 +1,14 @@
-#include <iostream>
+#include <stdio.h>
+#include<time.h>
 
-using namespace std;
 
-
-int main()
-{
-
-cout<<"Hello World";
+int main (){
+	
+	char state;
+	
+	printf("What do you want to do? (Read : r, Write: w,Find: f)\n");
+	scanf("%c", &state);
+	printf("Your state is %c",state);
 
 }
 
