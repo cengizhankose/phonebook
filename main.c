aafd7dd424b84de2c7c0f23ca5023e35aef68638
Groed <kose651@gmail.com>
2020-05-01 22:45:47 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include<stdlib.h>
+
 
 int main (){
 	
@@ -6,7 +8,17 @@ int main (){
 	
 	printf("What do you want to do? (Read : r, Write: w,Find: f)\n");
 	scanf("%c", &state);
-	printf("Your state is %c",state);
+	//printf("Your state is : %c",state);
+	
+	if( state == 'r' ){
+		printf("Your state is Read");
+	}else if( state == 'w' ){
+		printf("Your state is Write");
+	}else if(state == 'f' ){
+		printf("Your state is Find");
+	}else{
+		printf("İnvalid input");
+	}
 
 }
 
