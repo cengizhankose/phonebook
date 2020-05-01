7c8461796f16ee47a40f2174931ba375c90b67e6
Groed <kose651@gmail.com>
2020-05-02 01:12:08 +0300

    write fonksiyonuna isim ve numara ekleme özelliği ekledim read fonksiyonuna datayı okuyup ekrana bastırma özelliğini ekledim

--- main.c
+++ main.c
@@ -1,20 +1,68 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
 
+struct ContactDetail
+{
+	char name[50];
+	char phonenumber[50];
+}Contact;
 
 int read()
 {
-	
+	int i;
+	FILE *f;
+	f = fopen("data.txt","r");
+	if(f == NULL)
+	{
+		system("cls");
+		printf("No Contacts Available");
+		getch();
+		return -1;
+	}
+	system("cls");
+	printf("NAME");
+	printf("\tPHONE NUMBER\n");
+	for(i=0;i<35;i++)
+		printf("=");
+	while(fread(&Contact,sizeof(Contact),1,f)==1)
+	{
+		printf("\n%s\t%s",Contact.name,Contact.phonenumber);
+	}
+	puts("");
+	for(i=0;i<35;i++)
+		printf("=");	
+	fclose(f);
+	getch();
+	system("cls");
 }
 
 int write()
 {
+	char test;
 	FILE *f;
 	f = fopen("data.txt","a+");
-	if(f==0)
+	if(f==NULL)
 	{
 		f=fopen("data.txt","w+");
 	}
+	while(1)
+	{
+		printf("\n Enter Contact Details:");
+		printf("\n==========================\n");
+		printf("Enter Name:\n");
+		scanf("%s",Contact.name);
+		fflush(stdin);
+		printf("Enter Phonenumber:\n");
+		scanf("%s",Contact.phonenumber);
+		fwrite(&Contact,sizeof(Contact),1,f);
+		fflush(stdin);
+		printf("\n\nContact is successfully added!");
+		printf("\n Press esc key to exit,  any other key to add another contact:");
+		test=getche();
+		if(test==27)
+			break;		
+	}
+	fclose(f);
 }
 
 int find()
@@ -35,11 +83,13 @@ int main ()
 	{
 		case 'r':
 			system("cls");
-			printf("Your state is Read");
+			//printf("Your state is Read");
+			read();
 			break;
 		case 'w':
 			system("cls");
-			printf("Your state is Write");
+			//printf("Your state is Write");
+			write();
 			break;
 		case 'f':
 			system("cls");
